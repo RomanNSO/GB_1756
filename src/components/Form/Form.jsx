eaf619546856487bf81d5cb664e5ff6b80880f48
Roman <info>
2022-03-19 21:27:20 +0700

    add lesson-3

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,23 +1,32 @@
 import React, { useState } from "react";
-import styles from '../../styles.module.css'
+// import styles from '../../styles.module.css'
+import { Input, Button } from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
 
+export const Form = ({ addMessage }) => {
+  const [text, setText] = useState("");
 
-export const Form = ({addMessage}) => {
-    const [text, setText] = useState('');
+  const handleText = (ev) => {
+    ev.preventDefault();
+    addMessage({
+      text,
+      author: "User",
+    });
+    setText("");
+  };
 
-    const handleText = (ev) => {
-        ev.preventDefault();
-        addMessage({
-            text,
-            author: 'User',
-        });
-        setText('');
-    };
-
-    return (
-        <form onSubmit={handleText}>
-            <input className={styles.input} type="text" value={text} placeholder="Введите сообщение" onChange={ev => setText(ev.target.value)}/>
-            <button type="submit">Отправить</button>
-        </form>
-    );
-}; 
+  return (
+    <form onSubmit={handleText}>
+      {/* <input className={styles.input} type="text" value={text} placeholder="Введите сообщение" onChange={ev => setText(ev.target.value)}/> */}
+      <Input
+        value={text}
+        placeholder="Введите сообщение"
+        onChange={(ev) => setText(ev.target.value)}
+      />
+      {/* <button type="submit">Отправить</button> */}
+      <Button variant="contained" endIcon={<SendIcon />}>
+        Send
+      </Button>
+    </form>
+  );
+};
